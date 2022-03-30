d0866518e0eba8d1442d61046a7bda1494299f36
my_suc <my_suc@163.com>
2022-03-30 09:14:40 +0800

    我发起的-预付款/最终付款 增加撤回功能

--- src/api/project/apayment.js
+++ src/api/project/apayment.js
@@ -1,94 +1,118 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
 // 查询预付款列表
 export function listApayment(query) {
-  return request({
-    url: '/project/apayment/list',
-    method: 'get',
-    params: query
-  })
+    return request({
+        url: "/project/apayment/list",
+        method: "get",
+        params: query,
+    });
 }
 
 // 查询预付款详细
 export function getApayment(apyamentId) {
-  return request({
-    url: '/project/apayment/' + apyamentId,
-    method: 'get'
-  })
+    return request({
+        url: "/project/apayment/" + apyamentId,
+        method: "get",
+    });
 }
 
 // 新增预付款
 export function addApayment(data) {
-  return request({
-    url: '/project/apayment',
-    method: 'post',
-    data: data
-  })
+    return request({
+        url: "/project/apayment",
+        method: "post",
+        data: data,
+    });
 }
 
 // 修改预付款
 export function updateApayment(data) {
-  return request({
-    url: '/project/apayment',
-    method: 'put',
-    data: data
-  })
+    return request({
+        url: "/project/apayment",
+        method: "put",
+        data: data,
+    });
 }
 
 // 删除预付款
 export function delApayment(apyamentId) {
-  return request({
-    url: '/project/apayment/' + apyamentId,
-    method: 'delete'
-  })
+    return request({
+        url: "/project/apayment/" + apyamentId,
+        method: "delete",
+    });
 }
 // 查询项目信息列表
 export function getStList() {
-  return request({
-    url: '/project/st/listAll',
-    method: 'get'
-  })
+    return request({
+        url: "/project/st/listAll",
+        method: "get",
+    });
 }
 // 查询入库单列表
 export function getGrnList(data) {
-  return request({
-    url: '/project/grn/bydata',
-    method: 'get',
-    params: data
-  })
+    return request({
+        url: "/project/grn/bydata",
+        method: "get",
+        params: data,
+    });
 }
 // 查询出库单列表
 export function getGryList(data) {
-  return request({
-    url: '/project/gry/bydata',
-    method: 'get',
-    params: data
-  })
+    return request({
+        url: "/project/gry/bydata",
+        method: "get",
+        params: data,
+    });
 }
 
 // 查询委托合同
 export function getContract(data) {
-  return request({
-    url: '/project/contract/bydata',
-    method: 'get',
-    params: data
-  })
+    return request({
+        url: "/project/contract/bydata",
+        method: "get",
+        params: data,
+    });
 }
 
 // 查询全部首付款
 export function getApaymentbydata(data) {
-  return request({
-    url: '/project/apayment/bydata',
-    method: 'get',
-    params: data
-  })
+    return request({
+        url: "/project/apayment/bydata",
+        method: "get",
+        params: data,
+    });
 }
 
 // 初始化数据
 export function findInit(dataInit) {
-  return request({
-    url: '/project/apayment/findInit',
-    method: 'get',
-    params: dataInit
-  })
+    return request({
+        url: "/project/apayment/findInit",
+        method: "get",
+        params: dataInit,
+    });
+}
+
+// 验证出入库单状态是否可用
+export function checkSelnyStatusA(apyamentId) {
+    return request({
+        url: "/project/apayment/checkSelnyStatus/" + apyamentId,
+        method: "get",
+    });
+}
+
+// 重新绑定入/出库单
+export function reBindSelnyA(apyamentId) {
+    return request({
+        url: "/project/apayment/reBindSelny/" + apyamentId,
+        method: "post",
+    });
+}
+
+// 绑定入/出库单
+export function releaseSelnyA(apyamentId) {
+    return request({
+        url: "/project/apayment/releaseSelny/" + apyamentId,
+        method: "post",
+    });
 }
--- src/api/project/fpayment.js
+++ src/api/project/fpayment.js
@@ -1,109 +1,130 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
 // 查询最终付款列表
 export function listFpayment(query) {
-  return request({
-    url: '/project/fpayment/list',
-    method: 'get',
-    params: query
-  })
+    return request({
+        url: "/project/fpayment/list",
+        method: "get",
+        params: query,
+    });
 }
 
 // 查询最终付款详细
 export function getFpayment(fpaymentId) {
-  return request({
-    url: '/project/fpayment/' + fpaymentId,
-    method: 'get'
-  })
+    return request({
+        url: "/project/fpayment/" + fpaymentId,
+        method: "get",
+    });
 }
 
 // 新增最终付款
 export function addFpayment(data) {
-  return request({
-    url: '/project/fpayment',
-    method: 'post',
-    data: data
-  })
+    return request({
+        url: "/project/fpayment",
+        method: "post",
+        data: data,
+    });
 }
 
 // 修改最终付款
 export function updateFpayment(data) {
-  return request({
-    url: '/project/fpayment',
-    method: 'put',
-    data: data
-  })
+    return request({
+        url: "/project/fpayment",
+        method: "put",
+        data: data,
+    });
 }
 
 // 删除最终付款
 export function delFpayment(fpaymentId) {
-  return request({
-    url: '/project/fpayment/' + fpaymentId,
-    method: 'delete'
-  })
+    return request({
+        url: "/project/fpayment/" + fpaymentId,
+        method: "delete",
+    });
 }
 
 // 查询项目信息列表
 export function getStList() {
-  return request({
-    url: '/project/st/listAll',
-    method: 'get'
-  })
+    return request({
+        url: "/project/st/listAll",
+        method: "get",
+    });
 }
 
 // 查询出库单列表
 export function getGryList(data) {
-  return request({
-    url: '/project/gry/bydata',
-    method: 'get',
-    params: data
-  })
+    return request({
+        url: "/project/gry/bydata",
+        method: "get",
+        params: data,
+    });
 }
 
 // 查询委托合同
 export function getContract(data) {
-  return request({
-    url: '/project/contract/bydata',
-    method: 'get',
-    params: data
-  })
+    return request({
+        url: "/project/contract/bydata",
+        method: "get",
+        params: data,
+    });
 }
-
-
 
 // 查询是否有未回款物流与保证金
 export function getHkState(data) {
-  return request({
-    url: '/project/fpayment/HkState',
-    method: 'get',
-    params: data
-  })
+    return request({
+        url: "/project/fpayment/HkState",
+        method: "get",
+        params: data,
+    });
 }
-
 
 // 查询回款集合
 export function getOmList(data) {
-  return request({
-    url: '/project/om/bydata',
-    method: 'get',
-    params: data
-  })
+    return request({
+        url: "/project/om/bydata",
+        method: "get",
+        params: data,
+    });
 }
 
 // 结算服务费
 export function jsSp(data) {
-  return request({
-    url: '/project/fpayment/jsSp',
-    method: 'get',
-    params: data
-  })
+    return request({
+        url: "/project/fpayment/jsSp",
+        method: "get",
+        params: data,
+    });
 }
 
 // 查询开票列表
 export function getSticktList(data) {
-  return request({
-    url: '/project/sticket/bydata',
-    method: 'get',
-    params: data
-  })
+    return request({
+        url: "/project/sticket/bydata",
+        method: "get",
+        params: data,
+    });
+}
+
+// 验证出入库单状态是否可用
+export function checkSelnyStatusF(fpaymentId) {
+    return request({
+        url: "/project/fpayment/checkSelnyStatus/" + fpaymentId,
+        method: "get",
+    });
+}
+
+// 重新绑定入/出库单
+export function reBindSelnyF(fpaymentId) {
+    return request({
+        url: "/project/fpayment/reBindSelny/" + fpaymentId,
+        method: "post",
+    });
+}
+
+// 绑定入/出库单
+export function releaseSelnyF(fpaymentId) {
+    return request({
+        url: "/project/fpayment/releaseSelny/" + fpaymentId,
+        method: "post",
+    });
 }
